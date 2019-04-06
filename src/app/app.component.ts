95c72f3fa33465fa43aa9f403dfcf917487ac2dd
Prasanna Bhat <prasanna.k.bhat@gmail.com>
2019-04-06 23:32:43 +1100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eod-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'byeod';
+}
